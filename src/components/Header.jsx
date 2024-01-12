3dc634585b579f679664a560aabad43ce955850b
Red Monzon <redkmonzon30@gmail.com>
2024-01-12 00:00:44 -0600

    Create a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>CV Generator</h1>
+        <p>Unlock Your Career Potential with Our Cutting-Edge Resume Builder!</p>
+    </header>
+    </>
+  )
+}
+
+export default Header
